5be6303df205c690991b3df1b6b7bcd7a40f40ea
carolyn <93541667+cndavi@users.noreply.github.com>
2022-02-22 14:58:16 +0100

    update webshop

--- src/pages/Account/Account.jsx
+++ src/pages/Account/Account.jsx
@@ -1,5 +1,103 @@
+import styles from './Account.module.scss'
+import Footer from '../../components/Footer'
+import { useRef } from 'react'
+
 const Account = () => {
-  return <h1>Account</h1>
+  const emailRef = useRef()
+  const passwordRef = useRef()
+
+  return (
+    <>
+      <div className={styles.account}>
+        <div className={styles.account__image}>
+          <img
+            src={require('../../assets/images/heading/img3.jpg')}
+            alt='Records'
+          />
+        </div>
+        <div className={styles.account__content}>
+          <div className={styles.account__content__wrapper}>
+            <h1 className={styles.account__content__wrapper__title}>
+              MY ACCOUNT
+            </h1>
+            <div className={styles.account__content__wrapper__content}>
+              <div className={styles.account__content__wrapper__content__card}>
+                <div
+                  className={
+                    styles.account__content__wrapper__content__card__header
+                  }
+                >
+                  Login
+                </div>
+                <div
+                  className={
+                    styles.account__content__wrapper__content__card__content
+                  }
+                >
+                  <form className={styles.form}>
+                    <div className={styles.form__group}>
+                      <div className={styles.form__group__header}>
+                        <label>Email</label>
+                      </div>
+                      <input type='email' required ref={emailRef} />
+                    </div>
+                    <div className={styles.form__group}>
+                      <div className={styles.form__group__header}>
+                        <label>Password</label>
+                      </div>
+                      <input type='password' ref={passwordRef} required />
+                    </div>
+
+                    <div className={styles.form__group}>
+                      <div className={styles.form__group__space}>
+                        <div className={styles.form__group__space__item}>
+                          <label
+                            className={styles.form__group__space__item__label}
+                          >
+                            <div
+                              className={
+                                styles.form__group__space__item__checkbox
+                              }
+                            >
+                              REMEMBER ME
+                            </div>
+                          </label>
+                        </div>
+                        <div className={styles.form__group__space__item}>
+                          <div>FORGOT PASSWORD?</div>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className={styles.form__group}>
+                      <div className={styles.form__group__btn}>
+                        <button type='submit'>Login</button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+              <div className={styles.account__content__wrapper__content__card}>
+                <div
+                  className={
+                    styles.account__content__wrapper__content__card__header
+                  }
+                >
+                  Create account
+                </div>
+                <div
+                  className={
+                    styles.account__content__wrapper__content__card__content
+                  }
+                ></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
 }
 
 export default Account
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,5 +1,22 @@
+import styles from './Contact.module.scss'
+
 const Contact = () => {
-  return <h1>Contact</h1>
+  return (
+    <>
+      <section className={styles.header}>
+        <img
+          src={require('../../assets/images/heading/img2.jpg')}
+          alt='Records'
+        />
+      </section>
+      <div className={styles.contact}>
+        {/* <img
+          src={require('../../assets/images/heading/img4.jpg')}
+          alt='Records'
+        /> */}
+      </div>
+    </>
+  )
 }
 
 export default Contact
