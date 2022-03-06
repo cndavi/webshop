8c702e899556de0c141309b505f20039c142fff3
carolyn <93541667+cndavi@users.noreply.github.com>
2022-03-06 15:43:52 +0100

    update app and index

--- src/App.js
+++ src/App.js
@@ -6,29 +6,23 @@ import Account from './pages/Account'
 import Dashboard from './pages/Dashboard'
 import NotFoundPage from './components/NotFoundPage'
 import ScrollToTop from './utils/scrollToTop'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <Router>
-      <ScrollToTop>
-        <Navigation />
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route exact path='/products' element={<Products />} />
-          <Route exact path='/account' element={<Account />} />
-          <Route exact path='/contact' element={<Contact />} />
-          <Route exact path='/login' element={<Dashboard role='Login' />} />
-          <Route exact path='/reset' element={<Dashboard role='Reset' />} />
-          <Route
-            exact
-            path='/register'
-            element={<Dashboard role='Register' />}
-          />
-          <Route path='*' element={<NotFoundPage />} />
-        </Routes>
-      </ScrollToTop>
-    </Router>
+    <ScrollToTop>
+      <Navigation />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/products' element={<Products />} />
+        <Route exact path='/account' element={<Account />} />
+        <Route exact path='/contact' element={<Contact />} />
+        <Route exact path='/login' element={<Dashboard role='Login' />} />
+        <Route exact path='/reset' element={<Dashboard role='Reset' />} />
+        <Route exact path='/register' element={<Dashboard role='Register' />} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Routes>
+    </ScrollToTop>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
+import { BrowserRouter as Router } from 'react-router-dom'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
@@ -8,7 +9,9 @@ import './styles/index.scss'
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router basename='/webshop-web-cndavi'>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
